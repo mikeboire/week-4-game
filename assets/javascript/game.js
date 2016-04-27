43f7875562e11bc1facdf65a4dafbd8c205af1de
mikeboire <mike_boire@ymail.com>
2016-04-27 18:28:12 -0400

    finished
    mostly finished

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,18 +1,12 @@
-//globle variables
+
 var wins = 0;
 var losses = 0;
-//players currunt score
 var addition = 0;
-//selects a random number from 19 t0 120
 var total = Math.floor((Math.random()* 100) + 19);
-//assigns random value to crystals from 1-12
 var red = Math.floor((Math.random()* 12) + 1);
 var blue = Math.floor((Math.random()* 12) + 1);
 var yellow = Math.floor((Math.random()* 12) + 1);
 var green = Math.floor((Math.random()* 12) + 1);
-var audio = new Audio('https://files.slack.com/files-pri/T0J8XK2Q3-F0NHG3FDZ/download/raven.mp3');
-
-//updates addition in html
 var updateAddition = function (){
 	$('.addition').empty();
 	$('.addition').append(addition);
@@ -21,7 +15,6 @@ var updateAddition = function (){
 	$('#losses').empty();
 	$('#losses').append(losses);
 }
-//restart function
 var restart = function(){
 	addition = 0;
     total = Math.floor((Math.random()* 100) + 19);
@@ -33,7 +26,6 @@ var restart = function(){
     blue = Math.floor((Math.random()* 12) + 1);
     yellow = Math.floor((Math.random()* 12) + 1);
     green = Math.floor((Math.random()* 12) + 1);
-    //alert('restart');
     updateAddition();
 }
 var logic = function (){
@@ -54,7 +46,6 @@ var logic = function (){
 $('.total').append(total);
 
 $('.addition').append(addition);
-//function
 
 $(document).ready(function(){
 	$('#red').click(function(){
